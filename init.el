d8d923106c51414bc6c296dfeef309a6196bf95c
druiz17 <danteruiz11@yahoo.com>
2017-03-25 10:12:21 -0700

    added evil mode

--- init.el
+++ init.el
@@ -77,7 +77,7 @@
                            hungry-delete irony
                            let-alist levenshtein magit markdown-mode pkg-info
                            popup rtags seq solarized-theme vlf web-mode
-                           window-numbering writegood-mode yasnippet))
+                           window-numbering writegood-mode yasnippet doom-themes))
 ;; fetch the list of packages available
 (unless package-archive-contents
   (package-refresh-contents))
@@ -86,209 +86,9 @@
   (unless (package-installed-p package)
     (package-install package)))
 
-
-
-(ivy-mode 1)
-(projectile-global-mode)
-
-(setq projectile-known-projects-file
-      (concat user-cache-directory "projectile-bookmarks.eld"))
-(setq projectile-cache-file
-      (concat user-cache-directory "projectile.cache"))
-;; Enable projectile globally
-
-(setq projectile-completion-system 'ivy)
-(counsel-projectile-on)
-
-;; Require flycheck to be present
-(require 'flycheck)
-;; Force flycheck to always use c++11 support. We use
-;; the clang language backend so this is set to clang
-(add-hook 'c++-mode-hook
-          (lambda () (setq flycheck-clang-language-standard "c++11")))
-;; Turn flycheck on everywhere
-(global-flycheck-mode)
-
-;; Use flycheck-pyflakes for python. Seems to work a little better.
-(require 'flycheck-pyflakes)
-
-
-;; Load rtags and start the cmake-ide-setup process
-(require 'rtags)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Setup cmake-ide
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'cmake-ide)
-(cmake-ide-setup)
-;; Set cmake-ide-flags-c++ to use C++11
-(setq cmake-ide-flags-c++ (append '("-std=c++11")))
-;; We want to be able to compile with a keyboard shortcut
-(global-set-key (kbd "C-c m") 'cmake-ide-compile)
-
-
-;; Set rtags to enable completions and use the standard keybindings.
-;; A list of the keybindings can be found at:
-;; http://syamajala.github.io/c-ide.html
-(setq rtags-autostart-diagnostics t)
-(rtags-diagnostics)
-(setq rtags-completions-enabled t)
-(rtags-enable-standard-keybindings)
-
-
-(setq rtags-use-helm t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Package: yasnippet
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'yasnippet)
-;; To get a bunch of extra snippets that come in super handy see:
-;; https://github.com/AndreaCrotti/yasnippet-snippets
-;; or use:
-;; git clone https://github.com/AndreaCrotti/yasnippet-snippets.git ~/.emacs.d/yassnippet-snippets/
-(add-to-list 'yas-snippet-dirs "~/.emacs.d/yasnippet-snippets/")
-(yas-global-mode 1)
-(yas-reload-all)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Set up code completion with company and irony
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'company)
-(require 'company-rtags)
-(global-company-mode)
-
-;; Enable semantics mode for auto-completion
-(require 'cc-mode)
-(require 'semantic)
-(global-semanticdb-minor-mode 1)
-(global-semantic-idle-scheduler-mode 1)
-(semantic-mode 1)
-
-;; Setup irony-mode to load in c-modes
-(require 'irony)
-(require 'company-irony-c-headers)
-(require 'cl)
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'objc-mode-hook 'irony-mode)
-
-;; irony-mode hook that is called when irony is triggered
-(defun my-irony-mode-hook ()
-  "Custom irony mode hook to remap keys."
-  (define-key irony-mode-map [remap completion-at-point]
-    'irony-completion-at-point-async)
-  (define-key irony-mode-map [remap complete-symbol]
-    'irony-completion-at-point-async))
-
-(add-hook 'irony-mode-hook 'my-irony-mode-hook)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-
-;; company-irony setup, c-header completions
-(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
-;; Remove company-semantic because it has higher precedance than company-clang
-;; Using RTags completion is also faster than semantic, it seems. Semantic
-;; also provides a bunch of technically irrelevant completions sometimes.
-;; All in all, RTags just seems to do a better job.
-(setq company-backends (delete 'company-semantic company-backends))
-;; Enable company-irony and several other useful auto-completion modes
-;; We don't use rtags since we've found that for large projects this can cause
-;; async timeouts. company-semantic (after company-clang!) works quite well
-;; but some knowledge some knowledge of when best to trigger is still necessary.
-(eval-after-load 'company
-  '(add-to-list
-    'company-backends '(company-irony-c-headers
-                        company-irony company-yasnippet
-                        company-clang company-rtags)
-    )
-  )
-
-(defun my-disable-semantic ()
-  "Disable the company-semantic backend."
-  (interactive)
-  (setq company-backends (delete '(company-irony-c-headers
-                                   company-irony company-yasnippet
-                                   company-clang company-rtags
-                                   company-semantic) company-backends))
-  (add-to-list
-   'company-backends '(company-irony-c-headers
-                       company-irony company-yasnippet
-                       company-clang company-rtags))
-  )
-(defun my-enable-semantic ()
-  "Enable the company-semantic backend."
-  (interactive)
-  (setq company-backends (delete '(company-irony-c-headers
-                                   company-irony company-yasnippet
-                                   company-clang) company-backends))
-  (add-to-list
-   'company-backends '(company-irony-c-headers
-                       company-irony company-yasnippet company-clang))
-  )
-
-;; Zero delay when pressing C-tab
-(setq company-idle-delay 0)
-(define-key c-mode-map [(C-tab)] 'company-complete)
-(define-key c++-mode-map [(C-tab)] 'company-complete)
-;; Delay when idle because I want to be able to think
-(setq company-idle-delay 0.2)
-
-;; Prohibit semantic from searching through system headers. We want
-;; company-clang to do that for us.
-(setq-mode-local c-mode semanticdb-find-default-throttle
-                 '(local project unloaded recursive))
-(setq-mode-local c++-mode semanticdb-find-default-throttle
-                 '(local project unloaded recursive))
-
-(semantic-remove-system-include "/usr/include/" 'c++-mode)
-(semantic-remove-system-include "/usr/local/include/" 'c++-mode)
-(add-hook 'semantic-init-hooks
-          'semantic-reset-system-include)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Flyspell Mode for Spelling Corrections
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'flyspell)
-;; The welcome message is useless and can cause problems
-(setq flyspell-issue-welcome-flag nil)
-;; Fly spell keyboard shortcuts so no mouse is needed
-;; Use helm with flyspell
-(define-key flyspell-mode-map (kbd "<f8>") 'helm-flyspell-correct)
-;; (global-set-key (kbd "<f8>") 'ispell-word)
-(global-set-key (kbd "C-S-<f8>") 'flyspell-mode)
-(global-set-key (kbd "C-M-<f8>") 'flyspell-buffer)
-(global-set-key (kbd "C-<f8>") 'flyspell-check-previous-highlighted-word)
-(global-set-key (kbd "M-<f8>") 'flyspell-check-next-highlighted-word)
-;; Set the way word highlighting is done
-(defun flyspell-check-next-highlighted-word ()
-  "Custom function to spell check next highlighted word."
-  (interactive)
-  (flyspell-goto-next-error)
-  (ispell-word)
-  )
-
-;; Spell check comments in c++ and c common
-(add-hook 'c++-mode-hook  'flyspell-prog-mode)
-(add-hook 'c-mode-common-hook 'flyspell-prog-mode)
-
-;; Enable flyspell in text mode
-(if (fboundp 'prog-mode)
-    (add-hook 'prog-mode-hook 'flyspell-prog-mode)
-  (dolist (hook '(lisp-mode-hook emacs-lisp-mode-hook scheme-mode-hook
-                  clojure-mode-hook ruby-mode-hook yaml-mode
-                  python-mode-hook shell-mode-hook php-mode-hook
-                  css-mode-hook haskell-mode-hook caml-mode-hook
-                  nxml-mode-hook crontab-mode-hook perl-mode-hook
-                  tcl-mode-hook javascript-mode-hook))
-    (add-hook hook 'flyspell-prog-mode)))
-
-(dolist (hook '(text-mode-hook))
-  (add-hook hook (lambda () (flyspell-mode 1))))
-(dolist (hook '(change-log-mode-hook log-edit-mode-hook))
-  (add-hook hook (lambda () (flyspell-mode -1))))
-
-
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.cpp\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.hpp\\'" . c++-mode))
@@ -326,7 +126,7 @@
 
 
 
-(load-theme 'solarized-light t)
+(load-theme 'doom-one  t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
