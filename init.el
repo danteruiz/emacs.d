bfbe13b60367cb29f378e094582c5dc95e0d5ae0
druiz17 <danteruiz11@yahoo.com>
2017-05-05 22:02:33 -0700

    updating theme

--- init.el
+++ init.el
@@ -48,7 +48,7 @@
 
 (require 'package) ;; You might already have this line
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+	     '("melpa" . "https://melpa.org/packages/"))
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
@@ -65,19 +65,7 @@
 (package-initialize)
 ;; list the packages you want
 (defvar package-list)
-(setq package-list '(async auctex auto-complete autopair cmake-ide
-                           cmake-mode company company-irony
-			   counsel flyspell-correct-ivy
-			   counsel-projectile ivy
-			   projectile
-                           company-irony-c-headers dash epl flycheck
-                           flycheck-irony flycheck-pyflakes 
-                           helm helm-core helm-ctest
-                           helm-flycheck helm-flyspell helm-ls-git helm-ls-hg
-                           hungry-delete irony
-                           let-alist levenshtein magit markdown-mode pkg-info
-                           popup rtags seq solarized-theme vlf web-mode
-                           window-numbering writegood-mode yasnippet doom-themes))
+(setq package-list '(doom-themes moe-theme qml-mode))
 ;; fetch the list of packages available
 (unless package-archive-contents
   (package-refresh-contents))
@@ -86,61 +74,79 @@
   (unless (package-installed-p package)
     (package-install package)))
 
-(add-to-list 'load-path "~/.emacs.d/evil")
-(require 'evil)
-(evil-mode 1)
+(defun dts-switch-between-header-and-source ()
+  "Switch between a c/c++ header (.h) and its corresponding source (.c/.cpp)."
+  (interactive)
+  ;; grab the base of the current buffer's file name
+  (setq bse (file-name-sans-extension buffer-file-name))
+  ;; and the extension, converted to lowercase so we can
+  ;; compare it to "h", "c", "cpp", etc
+  (setq ext (downcase (file-name-extension buffer-file-name)))
+  ;; This is like a c/c++ switch statement, except that the
+  ;; conditions can be any true/false evaluated statement
+  (cond
+   ;; first condition - the extension is "h"
+   ((equal ext "h")
+    ;; first, look for bse.c
+    (setq nfn (concat bse ".c"))
+    (if (file-exists-p nfn)
+	;; if it exists, either switch to an already-open
+	;;  buffer containing that file, or open it in a new buffer
+	(find-file nfn)
+      ;; this is the "else" part - note that if it is more than
+      ;;  one line, it needs to be wrapped in a (progn )
+      (progn
+	;; look for a bse.cpp
+	(setq nfn (concat bse ".cpp"))
+	;; likewise
+	(find-file nfn)
+	)
+      )
+    )
+   ;; second condition - the extension is "c" or "cpp"
+   ((or (equal ext "cpp") (equal ext "c"))
+    ;; look for a corresponding bse.h
+    (setq nfn (concat bse ".h"))
+    (find-file nfn)
+    )
+   )
+  )
+(global-set-key (kbd "C-c s") 'dts-switch-between-header-and-source)
+
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.cpp\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.hpp\\'" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.qml\\'" . qml-mode))
 
+
+(add-hook 'before-save-hook 'white-space-cleanup)
 (add-hook 'c++-mode-hook
-          '(lambda ()
+	  '(lambda ()
 
-             ;; Use stroustrup style
-             (c-set-style "stroustrup")
+	     ;; Use stroustrup style
+	     (c-set-style "stroustrup")
 
-             ;; Setting indentation lvel
-             (setq c-basic-offset 4)
+	     ;; Setting indentation lvel
+	     (setq c-basic-offset 4)
 
-             ;; Make TAB equivilent to 4 spaces
-             (setq tab-width 4)
+	     ;; Make TAB equivilent to 4 spaces
+	     (setq tab-width 4)
 
-             ;; Use spaces to indent instead of tabs.
-             (setq indent-tabs-mode nil)
+	     ;; Use spaces to indent instead of tabs.
+	     (setq indent-tabs-mode nil)
 
-             ;; Indent the continuation by 2
-             (setq c-continued-statement-offset 2)
+	     ;; Indent the continuation by 2
+	     (setq c-continued-statement-offset 2)
 
-             ;; Brackets should be at same indentation level as the statements they open
-             ;; for example:
-             ;;                 if (0)        becomes        if (0)
-             ;;                     {                        {
-             ;;                        ;                         ;
-             ;;                     }                        }
-             (c-set-offset 'substatement-open 0)
+	     ;; Brackets should be at same indentation level as the statements they open
+	     ;; for example:
+	     ;;                 if (0)        becomes        if (0)
+	     ;;                     {                        {
+	     ;;                        ;                         ;
+	     ;;                     }                        }
+	     (c-set-offset 'substatement-open 0)
 
-             ;; make open-braces after a case
-             (c-set-offset 'case-label '+)
-             ))
-
-
-
-
-(load-theme 'doom-one  t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(package-selected-packages
-   (quote
-    (chicken-scheme yasnippet writegood-mode window-numbering web-mode vlf solarized-theme rtags markdown-mode magit hungry-delete helm-ls-hg helm-ls-git helm-flyspell helm-flycheck helm-ctest helm flycheck-pyflakes flycheck-irony flycheck dash company-irony-c-headers counsel-projectile flyspell-correct-ivy counsel company-irony company cmake-mode cmake-ide autopair auto-complete auctex async))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+	     ;; make open-braces after a case
+	     (c-set-offset 'case-label '+)
+	     ))
+(load-theme 'moe-dark t)
