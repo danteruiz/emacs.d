df109b71a91a5e1a5e75ac19dd187d3f2bc11687
Dante Ruiz <danteruiz102@gmail.com>
2018-03-08 10:49:41 -0800

    some better js syntax-highlighting

--- lisp/layers/js.el
+++ lisp/layers/js.el
@@ -1,4 +1,18 @@
-(add-hook 'js-mode-hook
-	  '(lambda ()
-	     (setq tab-width 4)
-	     (setq indent-tabs-mode nil)))
+
+(defvar javascript-packages
+  '(
+    js2-mode
+    ))
+
+(install-packages javascript-packages)
+
+(use-package js2-mode
+  :defer t
+  :init
+  (progn
+    (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+    (add-hook 'js-mode-hook
+	      '(lambda ()
+		 (setq tab-width 4)
+		 (setq indent-tabs-mode nil)
+		 (js2-imenu-extras-mode)))))
