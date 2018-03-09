e7962c084d6f88f5065524c2e2ba56f1a6561401
Dante Ruiz <danteruiz102@gmail.com>
2018-03-08 16:05:53 -0800

    fixing javascript

--- lisp/layers/js.el
+++ lisp/layers/js.el
@@ -6,13 +6,18 @@
 
 (install-packages javascript-packages)
 
+(defun js2-hook ()
+  (setq tab-width 4)
+  (setq indent-tabs-mode nil)
+  (js2-imenu-extras-mode)
+  (setq flycheck-check-syntax-automatically '(save idle-change))
+  (flycheck-mode))
+
+
 (use-package js2-mode
   :defer t
   :init
   (progn
+    (setq js2-idle-timer-delay 2)
     (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-    (add-hook 'js-mode-hook
-	      '(lambda ()
-		 (setq tab-width 4)
-		 (setq indent-tabs-mode nil)
-		 (js2-imenu-extras-mode)))))
+    (add-hook 'js-mode-hook 'js2-hook)))
