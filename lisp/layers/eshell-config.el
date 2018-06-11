07532dbb94e4145c787f0aabf7c353886264282e
druiz17 <danteruiz11@yahoo.com>
2018-06-10 21:37:24 -0700

    setting up my eshell

--- lisp/layers/eshell-config.el
+++ lisp/layers/eshell-config.el
@@ -2,6 +2,13 @@
 (defun eshell/custom-prompt ()
   )
 
+
+
+
+(defun eshell-hook ()
+  (eshell/alias "clear" "clear t"))
+
+
 (use-package eshell
   :defer t
   :config
@@ -13,4 +20,12 @@
 
   :init
   (progn
-    (message "initialize")))
+    (message "intiial eshell configuration")
+    (setq eshell-scroll-to-bottom-on-input 'all
+	  eshell-error-if-no-glob t
+	  eshell-hist-ignoredups t
+	  eshell-save-history-on-exit t
+	  eshell-prefer-lisp-functions nil)
+
+    (add-hook 'eshell-mode-hook 'eshell-hook)
+    ))
