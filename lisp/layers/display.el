42fc3dea6ce947726c3d4384d1b2ea8690c5f64d
danteruiz <danteruiz102@gmail.com>
2019-02-06 21:25:20 -0800

    adding spaceline

--- lisp/layers/display.el
+++ lisp/layers/display.el
@@ -1,7 +1,10 @@
 ;; display.el
 
-(defvar display-packages
-  '(all-the-icons
-    all-the-icons-dired))
+;;(defvar display-packages
+  ;;'(all-the-icons
+    ;;all-the-icons-dired))
 
-(install-packages display-packages)
+;;(install-packages display-packages)
+
+(require 'spaceline-config)
+(spaceline-spacemacs-theme)
