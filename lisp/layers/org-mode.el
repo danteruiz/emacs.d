26fb9b99cfb4283dfb4a31b345803613fcaaadc2
danteruiz <danteruiz102@gmail.com>
2020-03-22 12:32:27 -0700

    saving org files

--- lisp/layers/org-mode.el
+++ lisp/layers/org-mode.el
@@ -3,11 +3,15 @@
 ;;(add-to-list 'load-path (expand-file-name "~/github/org-mode/lisp"))
 ;;(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
 ;;(require 'org)
+
+(defvar org-mode-packages
+  `(org-projectile))
+
 (use-package org
   :init
   (progn
     (add-to-list 'auto-mode-alist '("\\.org'" . org-mode))
-    (setq org-agenda-files (quote ("~/github/orgfiles")))))
+    (setq org-agenda-files (quote ("~/org/")))))
 
 (use-package org-bullets
   :ensure t
