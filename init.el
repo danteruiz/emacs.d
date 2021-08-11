b3de3fc528372a87970029f23a2216cb11ad1695
Dante Ruiz <druiz@futurex.com>
2021-08-11 16:07:01 -0500

    adding spell check package

--- init.el
+++ init.el
@@ -1,8 +1,9 @@
-;;(setq debug-on-error t)
+;; (setq debug-on-error t)
 (load-file (concat (file-name-directory load-file-name)
                    "lisp/core/load-paths.el"))
 
 (require 'configure)
+(require 'subr-x)
 (configure/load-user-config-file)
 (configure/emacs-defaults)
 (configure/load-layers my-layers)
---
+++ lisp/layers/spellcheck.el
@@ -0,0 +1,7 @@
+
+(setq ispell-program-name "aspell")
+(setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US --camel-case"))
+(setq ispell-local-dictionary "en_US")
+(setq ispell-local-dictionary-alist
+      '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8)))
+(message "kjflskajdfkjasdf")
