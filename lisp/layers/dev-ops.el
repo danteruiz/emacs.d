fd9537c873ecf624a96c060ea71e70ad4a9eb074
Dante Ruiz <druiz@futurex.com>
2023-03-08 15:36:43 -0600

    Adding docker-compose-mode to dev-ops lisp module

--- lisp/layers/dev-ops.el
+++ lisp/layers/dev-ops.el
@@ -13,3 +13,10 @@
   :init
   (progn
     (add-to-list 'auto-mode-alist '("^Jenkinsfile'" . jenkinsfile-mode))))
+
+(use-package docker-compose-mode
+  :ensure t
+  :init
+  (progn
+    (add-to-list 'auto-mode-alist '("/docker-compose\\.yml\\'" . docker-compose-mode))
+    (add-to-list 'auto-mode-alist '("/docker-compose.override\\.yml\\'" . docker-compose-mode))))
